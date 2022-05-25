3f001b33bdd3259afa258b8dd61309200da424e5
Helen Cris <helencrisfernandes@hotmail.com>
2022-05-25 19:36:55 -0300

    FE-feat: Criação do circulo de background da home

--- supermercadosq/src/App.jsx
+++ supermercadosq/src/App.jsx
@@ -2,12 +2,15 @@ import { GlobalStyle } from "./styles/global";
 
 import { Header } from "./components/Header"
 
+import { Circle } from "./components/Circle"
+
 function App() {
 
   return (
     <>
       <Header />
       <GlobalStyle />
+      <Circle />
     </>
   )
 }
---
+++ supermercadosq/src/components/Circle/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ImgCircle } from './styles';
+
+export function Circle(){
+    return(
+      <ImgCircle />
+    )
+  }
