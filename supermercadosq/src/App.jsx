76951ad2454a62bf9779537f71710f6b19d33407
Victor Elias <98238941+svvictorelias@users.noreply.github.com>
2022-05-25 21:45:18 -0300

    Fe-Review: removido page404 e deixado Header

--- supermercadosq/src/App.jsx
+++ supermercadosq/src/App.jsx
@@ -1,14 +1,12 @@
 import { GlobalStyle } from "./styles/global";
 
 import { Header } from "./components/Header"
-import Page404 from "./components/Page404/Page404";
 
 function App() {
 
   return (
     <>
-      <Page404 />
-      {/* <Header />*/}
+      <Header />
       <GlobalStyle /> 
     </>
   )
