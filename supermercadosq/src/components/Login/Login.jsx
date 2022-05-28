9ba8b6d13cb3693f9999945900501c05448be007
Victor Elias <svvictorelias@gmail.com>
2022-05-27 22:22:56 -0300

    FE-03/feat finalizado CSS LoginPage

--- supermercadosq/src/components/Login/Login.jsx
+++ supermercadosq/src/components/Login/Login.jsx
@@ -5,41 +5,44 @@ import LogoSQ from '../../assets/Images/LogoSQ.png'
 const Login = () => {
   return (
     <StyleLogin>
-        <LoginContainer>
-            <LoginForm>
-                <h1>Entre</h1>
-                <p>E não perca mais nada sobre os vários produtos e seus respectivos componentes.</p>
-                <form>
-                  <label>
-                    <span>
-                        Email
-                    </span>
-                    <input type="email" name="email" id="email" />  
-                  </label>
-                  
-                  <label>
-                    <span>
-                        Senha
-                    </span>
-                    <input type="password" name="password" id="password" />  
-                  </label>
-                  <label id='keepConnected'>
-                    <input type="checkbox" name="keepConnected" id="keepConnected" />
-                    <span>
-                      Manter conectado
-                    </span>
-                  </label>
-                    <div className="actions">
-                      <span>Ainda não possui uma conta? <a href="">Criar Conta</a></span>
-                      <button>Entrar</button>
-                    </div>
-                </form>
-            </LoginForm>
+      <LoginContainer>
+        <LoginForm>
+          <h1>Entre</h1>
+          <p>
+            E não perca mais nada sobre os vários produtos e seus respectivos
+            componentes.
+          </p>
+          <form>
+            <label>
+              <span>Email</span>
+              <input type="email" name="email" id="email" />
+            </label>
+
+            <label>
+              <span>Senha</span>
+              <input type="password" name="password" id="password" />
+            </label>
             
-            <LoginLogo>
-                <img src={LogoSQ} alt="Logo do Supermercado SQ, dentro do Q possui um carrinho de compras, com algumas bolinhas nas cores rosa, laranja, azul, marrom e verde." />
-            </LoginLogo>
-        </LoginContainer>
+            <label id="keepConnected">
+              <input type="checkbox" name="keepConnected" id="keepConnected" />
+              <span>Manter conectado</span>
+            </label>
+            <div className="actions">
+              <span>
+                Ainda não possui uma conta? <a href="">Criar Conta</a>
+              </span>
+              <button>Entrar</button>
+            </div>
+          </form>
+        </LoginForm>
+
+        <LoginLogo>
+          <img
+            src={LogoSQ}
+            alt="Logo do Supermercado SQ, dentro do Q possui um carrinho de compras, com algumas bolinhas nas cores rosa, laranja, azul, marrom e verde."
+          />
+        </LoginLogo>
+      </LoginContainer>
     </StyleLogin>
   )
 }
