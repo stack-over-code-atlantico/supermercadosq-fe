192ea81b3855f463e072d4fd1b59799ccf1b4ac8
Helen Cris <helencrisfernandes@hotmail.com>
2022-05-30 18:32:06 -0300

    FE-feat:Feito a primeira parte do texto da home

--- supermercadosq/src/App.jsx
+++ supermercadosq/src/App.jsx
@@ -4,6 +4,8 @@ import { Header } from "./components/Header"
 
 import { Circle } from "./components/Circle"
 
+import { TextoBlog } from './components/TextoBlog'
+
 function App() {
 
   return (
@@ -11,6 +13,7 @@ function App() {
       <Header />
       <GlobalStyle />
       <Circle />
+      <TextoBlog />
     </>
   )
 }
