a6de114c6242d5c2758821c474636dd0897cfe8e
Ticianne <ticidias@gmail.com>
2022-05-30 19:56:50 -0300

    Feat add elements

--- supermercadosq/src/App.jsx
+++ supermercadosq/src/App.jsx
@@ -1,11 +1,12 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header"
+import SignIn from "./components/SignIn/Signin"
 
 function App() {
 
   return (
     <>
-      <Header />
+      <SignIn />
       <GlobalStyle /> 
     </>
   )
--- supermercadosq/src/components/SignIn/Signin.jsx
+++ supermercadosq/src/components/SignIn/Signin.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { LoginContainer, LoginLogo, StyleLogin } from './styles.jsx'
+import LogoSQ from '../../assets/Images/LogoSQ.png'
+import CellphoneCheck from './CellphoneCheck.jsx'
 
-const Signin = () => {
+const Login = () => {
   return (
-    <div>Signin</div>
+    <StyleLogin>
+      <LoginContainer>
+      <LoginLogo>
+          <img
+            src={LogoSQ}
+            alt="Logo do Supermercado SQ, dentro do Q possui um carrinho de compras, com algumas bolinhas nas cores rosa, laranja, azul, marrom e verde."
+          />
+        </LoginLogo>
+        <CellphoneCheck />
+      </LoginContainer>
+    </StyleLogin>
   )
 }
 
-export default Signin
+export default Login
