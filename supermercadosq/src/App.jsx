dfcfc88428280e4b57b17759287a2ae9a3b12f87
Ticianne <88796784+TicianneDias@users.noreply.github.com>
2022-05-30 21:10:17 -0300

    Update App.jsx

--- supermercadosq/src/App.jsx
+++ supermercadosq/src/App.jsx
@@ -1,12 +1,11 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header"
-import SignIn from "./components/SignIn/Signin"
 
 function App() {
 
   return (
     <>
-      <SignIn />
+      <Header />
       <GlobalStyle /> 
     </>
   )
