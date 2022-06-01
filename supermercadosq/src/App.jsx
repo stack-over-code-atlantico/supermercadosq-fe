68a5d738c0a8aca20c96fae03173e9873eb42f4f
Ticianne <ticidias@gmail.com>
2022-06-01 14:19:17 -0300

    FE/04 - feat Acrescentando botão para voltar para Pagina inicial no Login e SignIn e separação de rotas em um novo componente

--- supermercadosq/src/App.jsx
+++ supermercadosq/src/App.jsx
@@ -1,28 +1,14 @@
 import { GlobalStyle } from "./styles/global";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About/about";
-import Signin from "./pages/SignIn/Signin";
-import Login from "./pages/Login/Login";
-import {Product}  from "./pages/Products/product";
-import Page404 from "./pages/Page404/Page404"
-
+import { BrowserRouter as Router} from "react-router-dom";
+import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
   return (
   <>
-    <Router>
-      
-        <Routes>
-            <Route path="/" exact element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Signin/>} />
-            <Route path="/product" element={<Product/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/*" element={<Page404 />} />
-        </Routes>
-      </Router>
     <GlobalStyle />
+    <Router>
+      <AppRoutes/>
+    </Router>
   </>
   )
 }
--- supermercadosq/src/pages/SignIn/Signin.jsx
+++ supermercadosq/src/pages/SignIn/Signin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { LoginContainer, LoginLogo, StyleLogin } from './styles.jsx'
 import LogoSQ from '../../assets/Images/LogoSQ.png'
 import AdressCheck from './Inputs/AdressCheck'
-
+import { IoIosArrowBack } from 'react-icons/io';
 
 const SignIn = () => {
   return (
@@ -14,6 +14,7 @@ const SignIn = () => {
             src={LogoSQ}
             alt="Logo do Supermercado SQ, dentro do Q possui um carrinho de compras, com algumas bolinhas nas cores rosa, laranja, azul, marrom e verde."
           />
+        <span id='backHome'><IoIosArrowBack id='icon'/><a href="/">Página Inicial</a></span>
         </LoginLogo>
         <AdressCheck />
       </LoginContainer>
