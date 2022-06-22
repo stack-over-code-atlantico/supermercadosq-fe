c5b7fab1d614d2011a1998ba3272e3c7d221681a
Helen Cris <helencrisfernandes@hotmail.com>
2022-06-22 20:56:03 -0300

    Feat: Add about image

--- supermercadosq/src/components/About/index.jsx
+++ supermercadosq/src/components/About/index.jsx
@@ -1,4 +1,3 @@
-
 import React, { useContext } from 'react';
 import { AboutSection, AboutText, AboutParagraph } from './styles';
 import { LoadingScreen } from '../LoadingScreen/styles';
@@ -16,9 +15,11 @@ export function AboutComponent() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa fugit mollitia doloribus? Rerum optio molestiae
           itaque eveniet tempore provident ullam, reprehenderit aut qui, voluptatum vero eaque obcaecati ipsum mollitia eius.
         </AboutParagraph>
-        <LoadingScreen size='150px' background='#76BC44' left='15%' top='80%' text='Missão' />
-        <LoadingScreen size='150px' background='#3EBCD3' left='45%' top='80%' text='Visão'/>
-        <LoadingScreen size='150px' background='#f66a69' left='75%' top='80%' text='Valores'/>
+        <LoadingScreen size='140px' background='#F59E1D' left='5%' top='80%' text='União' />
+        <LoadingScreen size='140px' background='#76BC44' left='25%' top='80%' text='Missão' />
+        <LoadingScreen size='140px' background='#3EBCD3' left='45%' top='80%' text='Visão'/>
+        <LoadingScreen size='140px' background='#f66a69' left='65%' top='80%' text='Valores'/>
+        <LoadingScreen size='140px' background='#9cdbe7' left='85%' top='80%' text='Foco'/>
       </AboutSection>
     </>
   );
