1c23ff0ac6ecf611e006eee7540a59cf5821caa3
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2022-06-26 18:44:07 -0300

    feat/FE-06 - Incluindo os componentes NavbarAdm e AddProductCard na página Product

--- supermercadosq/src/pages/Product/Product.jsx
+++ supermercadosq/src/pages/Product/Product.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { NavbarProducts } from '../../components/NavbarProducts'
-// import { CardsProducts } from '../../components/CardsProducts'
+//import { CardsProducts } from '../../components/CardsProducts'
 import { Footer } from '../../components/Footer'
+import { NavbarAdm } from '../../components/NavbarProductsAdm'
+import { AddProductCard } from '../../components/AddProductCard'
 
 export function Product(){
   return (
     <>
+      <NavbarAdm/>
       <NavbarProducts />
+      <AddProductCard/>
       {/* <CardsProducts /> */}
       <Footer />
     </>
