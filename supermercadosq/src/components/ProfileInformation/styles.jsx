6327257ffac57c6bfafd49a1cd27a5ba1bebc449
DK <comododk@gmail.com>
2022-07-08 19:44:14 -0300

    Test

--- supermercadosq/src/components/ProfileInformation/styles.jsx
+++ supermercadosq/src/components/ProfileInformation/styles.jsx
@@ -7,6 +7,8 @@ export const ProfileInformationContainer = styled.div`
 export const ProfileInformationDiv = styled.div`
   width: 600px;
   background-color: pink;
-  /* height: 500px; */
+  height: 500px;
   margin-top: 7vh;
+
+
 `;
