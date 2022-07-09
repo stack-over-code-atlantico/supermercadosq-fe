4b1c713f3f0920612a72e6640259f5b6c0413ab1
Helen Cris <helencrisfernandes@hotmail.com>
2022-07-09 07:58:37 -0300

    Feat: indentado o componente passwordsecurity

--- supermercadosq/src/components/PasswordSecurity/PasswordSecurity.jsx
+++ supermercadosq/src/components/PasswordSecurity/PasswordSecurity.jsx
@@ -2,39 +2,36 @@ import React from 'react';
 import { ProfileInformationContainer } from './styles';
 import { useState } from "react";
 
-const PasswordSecurityForm = () => {
+export default PasswordSecurityForm = () => {
 
   const [senhaAntiga, setSenhaAntiga] = useState("");
   const [novaSenha, setNovaSenha] = useState("");
   const [repeteNovaSenha, setRepeteNovaSenha] = useState("");
 
+  return (
+    <>
+      <ProfileInformationContainer>
+        <h2 className="title">Editar senha</h2>
+        <h3 className="subtitle">Edite sua senha</h3>
+        <hr/>
+        <form >
+          <label>
+            Senha antiga:
+            <input type="password" value={senhaAntiga} onChange={e => setSenhaAntiga(e.target.value) } />
+          </label>
 
-    return (
-       <>
-        <ProfileInformationContainer>
-            <h2 className="title">Editar senha</h2>
-            <h3 className="subtitle">Edite sua senha</h3>
-            <hr/>
-            <form >
-              <label>
-                Senha antiga:
-                <input type="password" value={senhaAntiga} onChange={e => setSenhaAntiga(e.target.value) } />
-              </label>
+          <label>
+            Nova senha:
+            <input type="password" value={novaSenha} onChange={e => setNovaSenha(e.target.value) } />
+          </label>
 
-              <label>
-                Nova senha:
-                <input type="password" value={novaSenha} onChange={e => setNovaSenha(e.target.value) } />
-              </label>
-
-              <label>
-                Digite novamente a nova senha:
-                <input type="password" value={repeteNovaSenha} onChange={e => setRepeteNovaSenha(e.target.value) } />
-              </label>
-              <input id="button" type="submit" value="Salvar" />
-            </form>
-          </ProfileInformationContainer>
-       </>
-    );
+          <label>
+            Digite novamente a nova senha:
+            <input type="password" value={repeteNovaSenha} onChange={e => setRepeteNovaSenha(e.target.value) } />
+          </label>
+          <input id="button" type="submit" value="Salvar" />
+        </form>
+      </ProfileInformationContainer>
+    </>
+  );
 }
-
-export default PasswordSecurityForm;
