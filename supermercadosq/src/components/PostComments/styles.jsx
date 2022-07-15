e8eb31692cc9bc4c3fb86cb6f988d4d0de5d7066
Ticianne <ticidias@gmail.com>
2022-07-14 21:10:12 -0300

    Fix: Ajustes no style dos cards

--- supermercadosq/src/components/PostComments/styles.jsx
+++ supermercadosq/src/components/PostComments/styles.jsx
@@ -10,12 +10,12 @@ export const PostCommentsContainer = styled.div`
 
   .subtitle {
     margin-top: 4vh;
-    font-weight: 400;
     color: var(--color-blue-light);
+    font-weight: 400;
   }
 
   hr{
-    width: 450px;
+    width: auto;
     color: #E9E6E6;
   }
 `
@@ -24,7 +24,7 @@ export const Container = styled.main`
   display: flex;
   flex-wrap: wrap;
   width: 100%;
-  height: 100%;
+  height: 45%;
   li {
     list-style: none;
   }
@@ -33,44 +33,48 @@ export const Container = styled.main`
 export const CardList = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
 `;
 
 export const Card = styled.div`
   background: white;
   border-radius: 8px;
   box-shadow: 0 8px 16px #26395338;
-  width: 300px;
-  height: 180px;
+  width: 350px;
+  height: 200px;
   margin-top: 1rem;
-  overflow: hidden;
+  overflow-y: auto;
   margin-right: 1rem;
-  // Recua a imagem para o texto
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+   background-color: var(--color-blue-light);
+  }
+
   &:hover {
     #img {
       height: 100px;
     }
   }
-
-  hr {
-    color: #3EBCD3;
-  }
 `;
 
 export const TopCard = styled.div`
   padding: 20px 10px 5px 10px;
-  padding-top: 10px;
 `;
 
 export const Title = styled.h2`
   font-size: 1rem;
+  font-weight: 400;
   text-align: justify;
 `;
 
 export const SubTitle = styled.h3`
-  font-size: 0.8rem;
+  font-size: 0.7rem;
+  font-weight: 400;
 `;
 
 export const MediaCard = styled.div`
   padding: 5px 10px 5px 10px;
+  font-size: 0.9rem;
 `;
