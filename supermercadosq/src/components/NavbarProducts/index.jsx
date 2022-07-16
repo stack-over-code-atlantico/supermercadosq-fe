a9dce00e98619dfbf7fdc6cebc5f0847616f1097
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-07-16 13:03:51 -0300

    feat/4-FE: "Adicionando os redirecionamentos das Navbars"
    Adicionando os redirecionamentos, bem como o conteúdo da Navbar ( padrao/visitante ), fornecedor/cliente e administrador.

--- supermercadosq/src/components/NavbarProducts/index.jsx
+++ supermercadosq/src/components/NavbarProducts/index.jsx
@@ -1,23 +1,67 @@
 import React from 'react'
 import { DivNavbarProducts, NavbarList, ProductListItem } from './styles'
+import { useNavigate } from 'react-router-dom';
+import { userLevel } from '../../services/useAuth';
+import { NavbarAdm } from '../NavbarProductsAdm';
+import { logout } from '../../services/useAuth';
 
-export function NavbarProducts(){
-    return(
-        <DivNavbarProducts>
-            <NavbarList>
-                <ProductListItem>
-                    <a href='/'>Home</a>
-                </ProductListItem>
-                <ProductListItem>
-                    <a href='#'>Postagens</a>
-                </ProductListItem>
-                <ProductListItem>
-                    <a href='#'>Comentários</a>
-                </ProductListItem>
-                <ProductListItem>
-                    <a href='#'>Sair</a>
-                </ProductListItem>
-            </NavbarList>
-        </DivNavbarProducts>
+export function NavbarProducts() {
+    let navigate = useNavigate();
+
+    const handleNavigateHome = () => {
+        navigate('/', { replace: true });
+    }
+    const handleNavigateProfile = () => {
+        navigate('/profile', { replace: true });
+    }
+    const handleNavigateProduct = () => {
+        navigate('/product', { replace: true });
+    }
+
+    // Fazendo LogOff do usuario
+    const logOff = () => {
+        logout();
+        handleNavigateHome();
+    }
+
+    return (
+        <>
+            {
+                userLevel() && (userLevel().nivel === 'CLIENTE' || userLevel().nivel === 'FORNECEDOR') ? (
+                    <DivNavbarProducts>
+                        <NavbarList>
+                            <ProductListItem>
+                                <a href='' onClick={handleNavigateHome}>Home</a>
+                            </ProductListItem>
+                            <ProductListItem>
+                                <a href='' onClick={handleNavigateProfile}>Profile</a>
+                            </ProductListItem>
+                            <ProductListItem>
+                                <a href='' onClick={handleNavigateProduct}>Postagens</a>
+                            </ProductListItem>
+                            <ProductListItem>
+                                <a href='' onClick={logOff}>Sair</a>
+                            </ProductListItem>
+                        </NavbarList>
+                    </DivNavbarProducts>
+                ) : (
+                    <DivNavbarProducts>
+                        <NavbarList>
+                            <ProductListItem>
+                                <a href='' onClick={handleNavigateHome}>Home</a>
+                            </ProductListItem>
+                            <ProductListItem>
+                                <a href='' onClick={handleNavigateProduct}>Postagens</a>
+                            </ProductListItem>
+                        </NavbarList>
+                    </DivNavbarProducts>
+                )
+            }
+            {
+                userLevel() && userLevel().nivel === 'ADMINISTRADOR' ? (
+                    <NavbarAdm />
+                ) : (<></>)
+            }
+        </>
     )
 }
